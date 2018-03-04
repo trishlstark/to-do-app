06456be3ede14a2e3cc5f2e8511373f5279a3f5e
Trisha Stark <trishlstark@gmail.com>
2018-03-04 12:20:20 -0700

    Checkpoint 11 DOM Part 2 Complete

--- app.js
+++ app.js
@@ -1,52 +1,48 @@
 function onReady() {
+  let toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
 
-  addToDoForm.addEventListener('submit', event => {
-      event.preventDefault();
+  function createNewToDo(){
+    if (!newToDoText.value) { return; }
 
-    //get the text
-    let title = newToDoText.value;
-
-    //create a new li
-    let newLi = document.createElement('li');
-
-    //create a new input
-    let checkbox = document.createElement ('input');
-
-    //set the input's type to checkbox
-    checkbox.type="checkbox";
-
-    //set the title
-    newLi.textContent = title;
-
-    //attach the checkbox to the li
-    newLi.appendChild(checkbox);
-
-    //attach the li to the ul
-    toDoList.appendChild(newLi);
-
-    //empty the input
-    newToDoText.value='';
-
-    //create a new input
-    let deleteButton = document.createElement('input');
-
-    deleteButton.addEventListener('click', event => {
-      toDoList.removeChild(newLi);
+    toDos.push({
+      title: newToDoText.value,
+      complete: false
     });
+    newToDoText.value = '';
+    renderTheUI();
+  }
 
-    //set the input type to button
-    deleteButton.type = "button";
-    deleteButton.value = "delete";
+  function renderTheUI(){
+      const toDoList = document.getElementById('toDoList');
+      toDoList.textContent='';
 
-    //attach button to the li
-    newLi.appendChild(deleteButton);
+      toDos.forEach(function(toDo){
+        const newLi = document.createElement('li');
 
+        const checkbox = document.createElement('input');
+        checkbox.type = "checkbox";
+
+        newLi.textContent = toDo.title;
+
+        toDoList.appendChild(newLi);
+        newLi.appendChild(checkbox);
+      });
+  }
+
+  addToDoForm.addEventListener('submit', event =>{
+    event.preventDefault();
+    createNewToDo();
+    newToDoText.value = '';
   });
 
+  renderTheUI();
+
+
+
 }
+
 
 window.onload = function() {
   onReady();
